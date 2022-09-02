42bc1e784fbf1a5bca00eb819611014955c5adac
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2022-09-02 11:20:16 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  int a;
+  int b;
+  printf ("enter is a");
+  scanf("%d",&a);
+  printf("enter is b");
+  scanf("%d",&b)
+  
+
+  
+  return 0;
+}
