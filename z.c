5bb3682cc5160309db6d0c2a8c55a5f2828a1484
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2022-11-09 19:03:11 +0530

    Delete z.c

--- z.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("Hello prashant mishra \n");
-    printf(" how old are you \n");
-    printf("what are you doing now \n");
-    return 0;
-}
