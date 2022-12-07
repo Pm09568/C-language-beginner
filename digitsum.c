d7471d7adf61c34aae2277cdf01abcb906462d12
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2022-12-07 22:57:32 +0530

    Add files via upload

---
+++ digitsum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n ,m,sum=0;
+    printf("enter digit :");
+    scanf("%d",&n);
+
+    while(n>0)
+    {
+        m=n%10;
+        sum+=m;
+        n=n/10;
+    }
+    printf("Digit sum =%d",sum);
+}
