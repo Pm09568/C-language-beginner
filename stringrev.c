de5f7811bf606bc404857b826da9b3ef767ab657
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2023-01-24 18:40:44 +0530

    Update stringrev.c

--- stringrev.c
+++ stringrev.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-void displya(int *);
-int mian(){
+void displya(char *);
+int main(){
     char name[20];
     printf("enter string");
     scanf("%[^\n]",name);
@@ -11,12 +11,14 @@ int mian(){
           {
               int l=0,i;
               while(*name !='\0')
+                  
               {
                   name++;
                   l++;
               }
               name--;
-              for(int i=l-1;i>=0;i--,a--)
+              for(int i=l-1;i>=0;i--,name--)
               {
-                  printf("reverse string is %c ",*name)
+                  printf("%c",*name);
                   }
+          }
