2664c99817a6145e41865840319c09555bc0115a
Prashant mishra <110593606+Pm09568@users.noreply.github.com>
2023-01-24 18:32:54 +0530

    Update stringrev.c

--- stringrev.c
+++ stringrev.c
@@ -1,4 +1,22 @@
 #include<stdio.h>
+void displya(int *);
 int mian(){
     char name[20];
+    printf("enter string");
+    scanf(%[^\n]",name);
+          display(name);
+          return 0;
 }
+          void display(char *name)
+          {
+              int l=0,i;
+              while(*name !='\0')
+              {
+                  name++;
+                  l++;
+              }
+              name--;
+              for(int i=l-1;i>=0;i--,a--)
+              {
+                  printf("reverse string is %c ",*name)
+                  }
